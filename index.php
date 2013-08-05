6b1a37ad9a0961c8b752f7d436b990fb62a015c7
unknown <D@D-msi.(none)>
2013-08-04 22:05:15 -0700

    init

--- index.php
+++ index.php
@@ -94,8 +94,7 @@
         <li> <a href="#">Blog</a></li>
         <li> <a href="#">Projects</a></li>
         <li> <a href="#">Contact</a></li>
-        <li> <a href="#">Contact</a></li>
-        <li> 
+        <li> <a href="#">Resume</a></li>
       </ul>
     </div>
   </div>
