dc355d30f9ddf561a49d488be26a218e892ac1d2
unknown <D@D-msi.(none)>
2013-08-04 21:29:22 -0700

    init

--- index2.php
+++ index2.php
@@ -3,20 +3,15 @@
 <head>
   <title>Bootstrap In Action - Landing Page Example</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <!-- Bootstrap -->
   <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css" rel="stylesheet" />
   <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet" />
-
   <link href='http://fonts.googleapis.com/css?family=Abel|Open+Sans:400,600' rel='stylesheet'>
-
   <style>
-
   body {
     padding-top: 20px;
     font-size: 16px;
     font-family: "Open Sans",serif;
   }
-
   h1 {
     font-family: "Abel", Arial, sans-serif;
     font-weight: 400;
@@ -31,7 +26,6 @@
 
 </head>
 <body>
-
   <div class="container">
     <div class="row">
       <div class="col-lg-6 col-offset-3">
