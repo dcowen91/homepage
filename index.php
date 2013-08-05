c6eba2b195c2f8953699fb80055bfe3d9b53942b
unknown <D@D-msi.(none)>
2013-08-04 21:32:22 -0700

    init

--- index.php
+++ index.php
@@ -7,6 +7,14 @@
   <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet" />
   <link href='http://fonts.googleapis.com/css?family=Abel|Open+Sans:400,600' rel='stylesheet'>
   <style>
+
+  html {
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+  }
+
   body {
     padding-top: 20px;
     font-size: 16px;
