caa2a056e9cc23c584bf2ae561a676154f66a94f
unknown <D@D-msi.(none)>
2013-08-04 21:10:01 -0700

    init

--- index.php
+++ index.php
@@ -2,12 +2,17 @@
 
 <html lang="en">
     <head>
-        <meta charset="utf-8" />
         <title>Landing Page</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css" rel="stylesheet" />
         <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet" />
         <link href="http://fonts.googleapis.com/css?family=Abel|Open+Sans:400,600" rel="stylesheet" />
         <style>
+
+            html {
+                background: url()
+            }
+
             body  {
                 padding-top: 20px;
                 font-size: 16px;
