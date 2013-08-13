9ba6a675c1ca2e486f5489eedba9bc7ed4f83386
unknown <D@D-msi.(none)>
2013-08-13 16:18:22 -0700

    commit

---
+++ page2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Landing Page</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet" />
+  <link href='http://fonts.googleapis.com/css?family=Abel|Open+Sans:400,600' rel='stylesheet'>
+  <style>
+
+ 
+  .pull-right {
+  float: right;
+}
+
+  </style>
+
+</head>
+<body>
+  <div class="row" id="sidebar">
+    <ul class="inline">
+      <li> <a href="#">Home</a></li><li> <a href="#">Blog</a></li><li> <a href="#">Projects</a></li><li> <a href="#">Contact</a></li><li> <a href="#">Resume</a></li> 
+    </ul>
+  </div>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js" rel="script"></script>
+  <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/js/bootstrap.min.js" rel="script"></script>
+  <script type="text/javascript">
+
+  </script>
+
+</body>
+</html>
