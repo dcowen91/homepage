7ee921c2dd157c562cc09ae28e73920fea423f36
unknown <D@D-msi.(none)>
2013-08-13 00:48:16 -0700

    commit

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
   <title>Landing Page</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css" rel="stylesheet" />
+  <link src="bootstrap.min.css" rel="stylesheet" />
+  <link src="bootstrap.min.js" rel="script" />
   <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet" />
   <link href='http://fonts.googleapis.com/css?family=Abel|Open+Sans:400,600' rel='stylesheet'>
   <style>
