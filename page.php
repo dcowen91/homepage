c161366d195d39ce43ba93f448cb11e480529a10
unknown <D@D-msi.(none)>
2013-08-13 15:36:09 -0700

    commit

--- page.php
+++ page.php
@@ -3,9 +3,11 @@
 <head>
   <title>Landing Page</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link src="bootstrap.min.css" rel="stylesheet" />
+  <link href="bootstrap.min.css" rel="stylesheet" />
   <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet" />
   <link href='http://fonts.googleapis.com/css?family=Abel|Open+Sans:400,600' rel='stylesheet'>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js" rel="script"></script>
+  <script src="bootstrap.min.js" rel="script"></script>
   <style>
 
   html {
@@ -35,47 +37,38 @@
     margin: 40px 0;
   }
 
- </style>
+  </style>
 
 </head>
 <body>
+  <div id="sidebar">
+    <ul>
+      <li> <a href="#">Home</a></li>
+      <li> <a href="#">Blog</a></li>
+      <li> <a href="#">Projects</a></li>
+      <li> <a href="#">Contact</a></li>
+      <li> <a href="#">Resume</a></li> 
+    </ul>
+  </div>
   <div class="container">
-      <div id="sidebar">
-        <ul>
-          <li> <a href="#">Home</a></li>
-          <li> <a href="#">Blog</a></li>
-          <li> <a href="#">Projects</a></li>
-          <li> <a href="#">Contact</a></li>
-          <li> <a href="#">Resume</a></li> 
-          </ul>
-        </div>
+    <div class="row">
+      <div class="col-lg-6 col-offset-3 panel">
+        <h1 class="margin-base-vertical">Drew Owen</h1>
+        <p> 
+          I am a software engineer interested in all realms of development!
+        </p>
+        <p>
+          I study at the University of Arizona, seeking a degree in Computer Science. I will graduate in May 2014.
+        </p>
+        <p>
+          This is My "Home Page". As of now, there really isn't much to see. Don't assume that there isn't much to me though! More is to come to this venue.
+        </p>
       </div>
-    <div class="container">
-        <div class="row">
-          <div class="col-lg-6 col-offset-3" panel>
-
-            <h1 class="margin-base-vertical">Drew Owen</h1>
-
-            <p> 
-              I am a software engineer interested in all realms of development!
-            </p>
-
-            <p>
-              I study at the University of Arizona, seeking a degree in Computer Science. I will graduate in May 2014.
-            </p>
-
-            <p>
-              This is My "Home Page". As of now, there really isn't much to see. Don't assume that there isn't much to me though! More is to come to this venue.
-            </p>
-          </div>
-        </div>
-      </div>
-  </body>
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
-  <script src="bootstrap.min.js" rel="script"></script>
-
+    </div>
+  </div>
   <script type="text/javascript">
 
   </script>
 
-  </html>
+</body>
+</html>
