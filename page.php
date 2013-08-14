33980ce7866ab812c654e7e4bed06cbca31bd29b
unknown <D@D-msi.(none)>
2013-08-13 19:17:11 -0700

    commit

--- page.php
+++ page.php
@@ -60,8 +60,8 @@
 
 </head>
 <body>
-  <div class="row pull-right" id="sidebar">
-    <ul class="inline panel">
+  <div class="row pull-right panel" id="sidebar">
+    <ul class="inline">
       <li> <a href="#">Home</a></li><li> <a href="#">Blog</a></li><li> <a href="#">Projects</a></li><li> <a href="#">Contact</a></li><li> <a href="#">Resume</a></li> 
     </ul>
   </div>
