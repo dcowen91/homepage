f82452fb44de96206e174ffa9ef0e7e4b61923c2
unknown <D@D-msi.(none)>
2013-08-13 22:54:48 -0700

    commit

--- page.php
+++ page.php
@@ -95,7 +95,7 @@
     var date = new Date();
     var month = date.getMonth()+1;
     var year = date.getFullYear();
-    var day = date.getDate() - 2;
+    var day = date.getDate() - 5;
     if (day < 1) {
       day = 25;
       month--;
@@ -104,6 +104,10 @@
         year--;
       }
     }
+    if (day < 10)
+      day = "0" + day;
+    if (month < 10)
+      month = "0" + month;
     return year + "-" + month + "-" + day;
   }
 
