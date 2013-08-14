e1fd95f03232b3db511c7bf84a3833d246177cf1
unknown <D@D-msi.(none)>
2013-08-13 19:16:30 -0700

    commit

--- page.php
+++ page.php
@@ -42,7 +42,6 @@
 }
 
 .inline {
-  margin: 40px 0;
   padding-right: 20px;
 }
 
@@ -62,7 +61,7 @@
 </head>
 <body>
   <div class="row pull-right" id="sidebar">
-    <ul class="inline">
+    <ul class="inline panel">
       <li> <a href="#">Home</a></li><li> <a href="#">Blog</a></li><li> <a href="#">Projects</a></li><li> <a href="#">Contact</a></li><li> <a href="#">Resume</a></li> 
     </ul>
   </div>
