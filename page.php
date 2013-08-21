e739379e9b7c07e2acb2320d91de328f79862d94
unknown <Drew@Drew-PC.(none)>
2013-08-20 21:19:16 -0700

    commit

--- page.php
+++ page.php
@@ -94,10 +94,10 @@
           Feel free to look around, you might learn a thing or two about me!
         </p>
       </div>
-    </div>
-    <div id="button">
+          <div id="button">
       <button type="button" class="btn btn-default btn-primary">Hide</button>
       <button type="button" class="btn btn-default btn-primary">New Pic</button>
+    </div>
     </div>
   </div>
   <img id="hidden" src=""></img>
