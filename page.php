28e218b116b65ad39a126471912b9bf71166de87
unknown <Drew@Drew-PC.(none)>
2013-08-20 23:48:09 -0700

    commit

--- page.php
+++ page.php
@@ -105,6 +105,7 @@
     <button id="newpic" type="button" class="btn btn-default btn-primary">New Pic</button>
   </div>
   <img id="hidden" src=""></img>
+  <img src="/images/loading.gif" id="loading" style="display:none" />
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js" rel="script"></script>
   <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/js/bootstrap.min.js" rel="script"></script>
   <script type="text/javascript">
