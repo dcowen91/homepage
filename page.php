cf3a70f6337accd815289475bc8ca52b5645948f
unknown <Drew@Drew-PC.(none)>
2014-01-04 20:04:29 -0700

    commit

--- page.php
+++ page.php
@@ -1,17 +1,3 @@
-<?php
-
-    $dsn = "pgsql:host=ec2-54-197-241-64.compute-1.amazonaws.com;port=5432;dbname=d4qu50ed9jnsjp;user=ehyhduujrqfcnl;sslmode=require;password=P2ZzzgYBOUUc054rRGen8Y7YUb";
-    $db = new PDO($dsn);
-
-
-    // $users = $db->prepare("
-    // SELECT * FROM posts
-    // ");
-    // $posts->execute();
-    // $posts = $posts->fetchAll();
-
-
-?>
 <!DOCTYPE html>
 <html>
 <head>
