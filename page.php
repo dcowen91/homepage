c2fc3e5bc25465f3e10ea5239a5431f5af309f8c
unknown <Drew@Drew-PC.(none)>
2014-01-13 19:16:48 -0700

    commit

--- page.php
+++ page.php
@@ -261,7 +261,7 @@
     $('#blog').show();
     $('#leftPost').show();
     $('#rightPost').show(); 
-    $('#blog').html('<h1 class="margin-base-vertical">' + message['title'] + '</h1> <p>' + message['content'] + '</p>' + '<button id="leftPost" type="button" class="btn btn-default btn-primary">Next</button> <button id="rightPost" type="button" class="btn btn-default btn-primary">Prev</button>');
+    $('#blog').html('<h1 class="margin-base-vertical">' + message['title'] + '</h1> <p>' + message['content'] + '</p>' + 'posted at ' + message['posted'] + '<button id="leftPost" type="button" class="btn btn-default btn-primary">Next</button> <button id="rightPost" type="button" class="btn btn-default btn-primary">Prev</button>');
   }
 
   function getPost(postnum) {
