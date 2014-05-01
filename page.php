9b37bf4fb86a5ab7b02f0eca4165165d59fd8c31
Drew Owen <dcowen@email.arizona.edu>
2014-04-30 21:44:14 -0700

    removed chief

--- page.php
+++ page.php
@@ -11,6 +11,8 @@
   <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet" />
   <link href='http://fonts.googleapis.com/css?family=Abel|Open+Sans:400,600' rel='stylesheet'>
   <link rel="stylesheet" type="text/css" href="page.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js" rel="script"></script>
+  <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/js/bootstrap.js" rel="script"></script>
 
 </head>
 <body>
@@ -52,8 +54,6 @@
   </div>
   <img id="hidden" src="images/Chief.png" alt="background">
   <img src="images/Loading.gif" id="loading" alt="loading" style="display:none">
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js" rel="script"></script>
-  <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/js/bootstrap.js" rel="script"></script>
   <script type="text/javascript">
 
   var pics;
@@ -146,7 +146,7 @@
   });
 
 
-  $( document ).ready(function() {
+  $(document).ready(function() {
     //console.log("ready")
     var currentState = window.location.hash;
     if (currentState.indexOf("#") != -1) {
