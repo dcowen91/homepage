e9c87feb75d0a4ab282fddc2becae253d9f90b1f
Drew Owen <dcowen@email.arizona.edu>
2014-05-02 03:07:37 -0700

    tr to fix disqus

--- page.php
+++ page.php
@@ -54,7 +54,7 @@
                             this.page.url = newUrl;
                         }
                     });
-                });
+                };
               </script>
             <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
             <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
@@ -325,7 +325,7 @@
             this.page.url = newUrl;
         }
     });
-  });
+  };
 
 
   </script>
