170c2de439fc2608ffd72eecd78dff1f06d01151
Drew Owen <dcowen@email.arizona.edu>
2014-05-19 14:57:59 -0700

    what the heck just happened to my blog

--- index.php
+++ index.php
@@ -269,7 +269,8 @@ $_GET['url'] = $_SERVER['REQUEST_URI'];
     $('#rightPost').show();
     $('#blogInner').show();
     var posted = new Date(message['posted']);
-    $('#blogInner').html('<button id="blogIndex" onclick="blogIndex()" type="button" class="btn btn-primary btn-xs"><i class="fa fa-align-justify"> Index</button>' +'<h1 class="margin-base-vertical">' + message['title'] + '</h1> <p>' + message['content'] +  
+    $('#blogInner').html('<button id="blogIndex" onclick="blogIndex()" type="button" class="btn btn-primary btn-xs">Index</button>' +
+      '<h1 class="margin-base-vertical">' + message['title'] + '</h1> <p>' + message['content'] +  
       '</p> <small> <em>' + 'posted on ' + posted.toLocaleDateString() + ' at ' + posted.toLocaleTimeString() + 
       '</br> </small> </em> ' + '<button id="leftPost" type="button" onclick="leftClick()" class="btn btn-primary btn-xs">' + 
       '<i class="fa fa-chevron-left"></i> Next</button> <button id="rightPost" onclick="rightClick()" type="button" class="btn btn-primary btn-xs"> <i class="fa fa-chevron-right"></i> Prev</button>');  
